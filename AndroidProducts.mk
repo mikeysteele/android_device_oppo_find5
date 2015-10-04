0163bcd4f7e04e3b5bbd8625f6b01b74657f5f35
Michael Westcott <mjwestcott@gmail.com>
2015-10-04 19:51:20 +1100

    initial bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2008 The Android Open Source Project
+# Copyright 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,5 +14,6 @@
 # limitations under the License.
 #
 
-add_lunch_combo full_find5-userdebug
-add_lunch_combo omni_find5-userdebug
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_find5.mk \
+    $(LOCAL_DIR)/omni_find5.mk
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_SPECIFIC_HEADER_PATH := device/oppo/find5/include
 TARGET_BOARD_INFO_FILE := device/oppo/find5/board-info.txt
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_x909_defconfig
+#TARGET_KERNEL_CONFIG := cyanogenmod_x909_defconfig
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oppo/find5/bluetooth
@@ -30,8 +30,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oppo/find5/bluetooth
 # Camera
 TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
-# CM  Hardware
-BOARD_HARDWARE_CLASS += device/oppo/find5/cmhw
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
 BOARD_CACHEIMAGE_PARTITION_SIZE := 536870912
@@ -40,5 +38,10 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 
+
 # inherit from the proprietary version
 -include vendor/oppo/find5/BoardConfigVendor.mk
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/oppo/find5/configs/fstab.find5
+
